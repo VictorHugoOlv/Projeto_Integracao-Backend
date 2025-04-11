aa4b65818a535df4b07dbdedec5695c89d67a497
Victor Hugo <victor.hugo.olv@gmail.com>
2025-04-11 15:13:10 -0300

    Alteração: getDistinctLinesSorted agora rodando em LineController

--- src/main/java/org/example/controllers/LineController.java
+++ src/main/java/org/example/controllers/LineController.java
@@ -5,15 +5,26 @@ import org.example.models.Line;
 
 import org.hibernate.Session;
 import org.hibernate.query.Query;
+
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LineController {
-
 
     public List<Line> getAllLines() {
         try (Session session = DataBaseHelper.getSession()) {
             Query<Line> query = session.createQuery("FROM Line", Line.class);
             return query.getResultList();
         }
+    }
+
+    public Set<String> getDistinctLinesSorted() {
+        return getAllLines().stream()
+                .map(Line::getLineName)
+                .filter(line -> !line.isEmpty())
+                .sorted()
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
