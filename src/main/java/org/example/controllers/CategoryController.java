9dec03717c4630ef19f94aa574c2844c7b648f61
Victor Hugo <victor.hugo.olv@gmail.com>
2025-04-15 15:27:41 -0300

    Alteração: remoção do try no método getCategoriesByLine

--- src/main/java/org/example/controllers/CategoryController.java
+++ src/main/java/org/example/controllers/CategoryController.java
@@ -6,18 +6,18 @@ import org.example.models.Category;
 import org.example.models.Line;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
+
 import java.util.List;
 
 public class CategoryController {
 
     public List<Category> getCategoriesByLine(Line line) {
-        try (Session session = DataBaseHelper.getSession()) {
-            Query<Category> query = session.createQuery(
-                    "SELECT DISTINCT c FROM Category c LEFT JOIN FETCH c.products " +
-                            "WHERE c.line = :line", Category.class
-            );
-            query.setParameter("line", line);
-            return query.list();
-        }
+        Session session = DataBaseHelper.getSession();
+        Query<Category> query = session.createQuery(
+                "SELECT DISTINCT c FROM Category c LEFT JOIN FETCH c.products " +
+                        "WHERE c.line = :line", Category.class
+        );
+        query.setParameter("line", line);
+        return query.list();
     }
 }
