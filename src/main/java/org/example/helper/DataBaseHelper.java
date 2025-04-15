66d8e2d37cec9c1e77bdffc72a14d5114e8dbbb1
Victor Hugo <victor.hugo.olv@gmail.com>
2025-04-15 15:50:53 -0300

    Alteração: Controle de sessão implementado de forma correta

--- src/main/java/org/example/helper/DataBaseHelper.java
+++ src/main/java/org/example/helper/DataBaseHelper.java
@@ -3,13 +3,19 @@ package org.example.helper;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.example.utils.HibernateUtil;
+import static org.example.utils.HibernateUtil.threadLocalSession;
 
 public class DataBaseHelper {
 
     private static final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
     public static Session getSession() {
-        return sessionFactory.openSession();
+        Session session = threadLocalSession.get();
+        if (session == null || !session.isOpen()) {
+            session = sessionFactory.openSession();
+            threadLocalSession.set(session);
+        }
+        return session;
     }
 
 }
--- src/main/java/org/example/utils/HibernateUtil.java
+++ src/main/java/org/example/utils/HibernateUtil.java
@@ -1,10 +1,13 @@
 package org.example.utils;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
+
+    public static final ThreadLocal<Session> threadLocalSession = new ThreadLocal<>();
 
     static {
         try {
