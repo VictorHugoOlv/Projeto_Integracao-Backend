98b1e5cd06fbaf80e62edf6d0f6e84baa053ac44
Victor Hugo <victor.hugo.olv@gmail.com>
2025-06-05 11:04:24 -0300

    Criação: Controllers de teste criados

--- src/test/java/org/example/controllers/CategoryControllerTest.java
+++ src/test/java/org/example/controllers/CategoryControllerTest.java
@@ -1,4 +1,29 @@
 package org.example.controllers;
 
-public class CategoryControllerTest {
+import org.example.services.CategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryControllerTest {
+
+    @Mock
+    private CategoryService categoryService;
+
+    @InjectMocks
+    private CategoryController categoryController;
+
+    @Test
+    @DisplayName("Conseguir pegar todas as categorias e produtos de Cronos")
+    void getCategoriesByLineTest() {
+        categoryController.getCategoriesByLine(1L);
+
+        verify(categoryService, times(1)).getCategoriesByLine(1L);
+    }
 }
--- src/test/java/org/example/controllers/LineControllerTest.java
+++ src/test/java/org/example/controllers/LineControllerTest.java
@@ -1,4 +1,27 @@
 package org.example.controllers;
 
-public class LineControllerTest {
+import org.example.services.LineService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class LineControllerTest {
+
+    @Mock
+    private LineService lineService;
+
+    @InjectMocks
+    private LineController lineController;
+
+    @Test
+    void getLinesTest() {
+        lineController.getAllLines();
+
+        verify(lineService, times(1)).getAllLines();
+    }
 }
